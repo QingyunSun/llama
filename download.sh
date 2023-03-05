2d10bef6700cbb015205d085a457a9e7ef3de6f9
QingyunSun <qysun@stanford.edu>
2023-03-05 02:11:30 -0700

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,9 +1,9 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # This software may be used and distributed according to the terms of the GNU General Public License version 3.
 
-PRESIGNED_URL=""             # replace with presigned url from email
+PRESIGNED_URL="https://dobf1k6cxlizq.cloudfront.net/*?Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaHR0cHM6Ly9kb2JmMWs2Y3hsaXpxLmNsb3VkZnJvbnQubmV0LyoiLCJDb25kaXRpb24iOnsiRGF0ZUxlc3NUaGFuIjp7IkFXUzpFcG9jaFRpbWUiOjE2NzgzNjg2NTl9fX1dfQ__&Signature=djKQz1p1p6Uzpfa1pbihXRNDKMty-BOEIxSNoWEpvafiCMFl5z70FEr2RpSjDxSXVvuzNXskEwx9gqRM5JQ2GJsKaKVm5ps9u4GPyuyvZPFL~jCW4d9wTLCfy84cnzhBIsJNElLnR60FiaVSln0DRbIwzwbxFI-2ZGcMg9gJ8XZIJT8XeLHrYGx6SlrqEXuHrzDdbsDj-KCQjcUAmd17B7QWWkrzb2rPbAyzOaNiBTaiWtrrRjY0xOqy4N2AP0HXdwGOBxkNKJzpqlFhZ1koydnDXJlOsOkahPLBCcUPtsjSuSKb4QtJ6jq2vqBt5LmyY1MUDbunRthyvZdRtbLDmg__&Key-Pair-Id=K231VYXPC1TA1R"             # replace with presigned url from email
 MODEL_SIZE="7B,13B,30B,65B"  # edit this list with the model sizes you wish to download
-TARGET_FOLDER=""             # where all files should end up
+TARGET_FOLDER="/Users/qingyunsun/Dropbox/LLMA"             # where all files should end up
 
 declare -A N_SHARD_DICT
 
